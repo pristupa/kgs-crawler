0c0328d8de840816a0dce580fc786c6258c885dd
Pavel Pristupa <pristupa@gmail.com>
2019-03-09 18:41:25 +0700

    Add logging

--- src/kgs_client.py
+++ src/kgs_client.py
@@ -6,11 +6,13 @@ class KGSClient:
 
     def download_month_archive(self, nickname: str, year: int, month: int) -> bytes:
         url = self.BASE_URL + f'servlet/archives/ru_RU/{nickname}-{year}-{month}.zip'
+        print(f'Downloading {url}')
         # TODO: Handle 404
         with urllib.request.urlopen(url) as zip_file:
             return zip_file.read()
 
     def get_archives_page(self, nickname: str) -> bytes:
         url = self.BASE_URL + f'gameArchives.jsp?user={nickname}'
+        print(f'Downloading {url}')
         with urllib.request.urlopen(url) as archives_page:
             return archives_page.read()
